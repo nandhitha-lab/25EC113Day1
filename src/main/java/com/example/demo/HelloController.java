0feeb1bd8c0fcfad5978346d07ecc302eac0b373
nandhitha-lab <nandhitha.b2025ece@sece.ac.in>
2026-09-16 23:22:07 +0530

    Initial commit - Spring Boot HelloController

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld(@RequestParam String name) {
+        return "Hello World " + name;
+    }
+
+    @PostMapping("/")
+    public String helloWorldPost(@RequestParam String name) {
+        return "Account created for " + name;
+    }
+}
